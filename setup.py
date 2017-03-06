304699be3e204c03ab1ad8cb2b702d0ffd39b8b2
Erik Reppel <erikreppel@coinbase.com>
2017-03-06 15:25:47 -0800

    Fix bug in install

--- setup.py
+++ setup.py
@@ -11,19 +11,15 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
-
 setup(
     name='otpad',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.0',
+    version='0.1.1',
 
     description='A small util for doing one-time pad',
-    long_description=long_description,
 
     # The project's main homepage.
     url='https://github.com/erikreppel/otp',
