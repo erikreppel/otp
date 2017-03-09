bb12668e8706e25cba2206ce067da76b9f64b81b
Erik Reppel <erikreppel@coinbase.com>
2017-03-08 17:41:17 -0800

    Refactor to have optional hmac verification

--- otpad/otpad.py
+++ otpad/otpad.py
@@ -1,20 +1,67 @@
 from operator import xor
+import hmac
+import hashlib
+import base64
 
 
-def onetimepad(key1, key2):
+def pad(key, key_to_encrypt, hmac_key=None):
     '''
     Performs a one time pad on two strings.
-    :params - key1: key 1 and key 2 are strings of equal length
-            - key2: key 1 and key 2 are strings of equal length
-    :returns - the resultant from the XOR as a string
-    Note: key1 and key2 must be the same length
-          and that operations are symmetric
+    :params - key: key and key_to_encrypt are strings of equal length
+            - key_to_encrypt: key and key_to_encrypt are strings of equal len
+            - hmac_key: if a key is present, will add key 'digest' to return dictionary
+    :returns - dictionary with key 'encrypted' which is the resultant from the XOR as a string.
+               If :param hmac_key is present, will also have key 'digest' in return dictionary
     '''
-    assert len(key1) == len(key2)
-    key1_bytes, key2_bytes = bytearray(key1), bytearray(key2)
+    assert len(key) == len(key_to_encrypt)
+    key1_bytes, key2_bytes = bytearray(key), bytearray(key_to_encrypt)
     padded = bytearray([1] * len(key1_bytes))
 
     for i, _ in enumerate(key1_bytes):
         padded[i] = xor(key1_bytes[i], key2_bytes[i])
-    padded = padded.decode('utf-8')
-    return str(padded)
+    padded = str(padded.decode('utf-8'))
+
+    if not hmac_key:
+        return {'encrypted': padded}
+
+    digest = hmac.new(hmac_key, msg=padded, digestmod=hashlib.sha256).digest()
+    digest = base64.b64encode(digest)
+    return {'encrypted': padded, 'digest': digest}
+
+
+def unpad(key, encrypted_key, hmac_key=None, hmac_digest=None):
+    '''
+    Performs a one time pad on two strings and optionally verifies the hmac.
+    :params - key: key and encrypted_key are strings of equal length
+            - encrypted_key: key and encrypted_key are strings of equal length
+            - hmac_key: if a key is present, will verify and throw exception if not valid
+    :returns - dictionary with key 'decrypted' which is the resultant from the XOR as a string.
+    '''
+    assert len(key) == len(encrypted_key)
+
+    if hmac_key:
+        if not hmac_digest:
+            raise Exception('hmac_key with no hmac_digest')
+        digest = hmac.new(hmac_key, msg=encrypted_key, digestmod=hashlib.sha256).digest()
+        digest = base64.b64encode(digest)
+        if _safe_string_compare(digest, hmac_digest) is False:
+            raise Exception('computed hmac of encrypted_key does not match the hmac_digest')
+
+    key1_bytes, key2_bytes = bytearray(key), bytearray(encrypted_key)
+    padded = bytearray([1] * len(key1_bytes))
+
+    for i, _ in enumerate(key1_bytes):
+        padded[i] = xor(key1_bytes[i], key2_bytes[i])
+    padded = str(padded.decode('utf-8'))
+    return {'decrypted': padded}
+
+
+def _safe_string_compare(string1, string2):
+    same = True
+    max_length = max(len(string1), len(string2))
+    for i in range(max_length):
+        if len(string1) <= i or len(string2) <= i:
+            continue
+        if string1[i] != string2[i]:
+            same = False
+    return same
--- otpad/test_onetimepad.py
+++ otpad/test_onetimepad.py
@@ -1,5 +1,5 @@
 import unittest
-from onetimepad import onetimepad
+import otpad
 from cryptography.fernet import Fernet
 
 
@@ -7,19 +7,24 @@ class TestOnetimepad(unittest.TestCase):
 
     def test_string_lengths_must_match(self):
         with self.assertRaises(AssertionError):
-            onetimepad('abc', 'defg')
+            otpad.pad('abc', 'defg')
+            otpad.unpad('abc', 'defg')
 
     def test_keys_are_retrievable(self):
         k1 = Fernet.generate_key()
         k2 = Fernet.generate_key()
-        encrypted = onetimepad(k1, k2)
-        self.assertEqual(k1, onetimepad(encrypted, k2))
+        encrypted = otpad.pad(k1, k2)
+        self.assertEqual(k2,
+                         otpad.unpad(k1, encrypted['encrypted'])['decrypted'])
 
-    def test_symmetry_of_onetime_pad(self):
+    def test_hmac_encryption_and_retrieval(self):
         k1 = Fernet.generate_key()
         k2 = Fernet.generate_key()
-        encrypted = onetimepad(k1, k2)
-        self.assertEqual(k1, onetimepad(encrypted, k2))
-        self.assertEqual(k1, onetimepad(k2, encrypted))
-        self.assertEqual(k2, onetimepad(k1, encrypted))
-        self.assertEqual(k2, onetimepad(encrypted, k1))
+        k3 = Fernet.generate_key()
+
+        padded = otpad.pad(k1, k2, hmac_key=k3)
+        orig = otpad.unpad(k1,
+                           padded['encrypted'],
+                           hmac_key=k3,
+                           hmac_digest=padded['digest'])
+        self.assertEqual(orig['decrypted'], k2)
